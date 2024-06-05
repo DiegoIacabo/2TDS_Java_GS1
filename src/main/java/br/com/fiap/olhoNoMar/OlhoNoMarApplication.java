0fa2f2b855f951ece6bcd0c746dbfba7aac80668
Diego <diegoiacabo@gmail.com>
2024-06-04 23:17:57 -0300

    Criação do projeto

---
+++ src/main/java/br/com/fiap/olhoNoMar/OlhoNoMarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.olhoNoMar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlhoNoMarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlhoNoMarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/olhoNoMar/OlhoNoMarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.olhoNoMar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlhoNoMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
